5f89cdf257311050c8ff7a7d013f47e5edbff736
SakyJoestar <itzsaky@outlook.com>
2023-05-13 12:20:52 -0500

    Configuracion proyecto completada

--- build.sbt
+++ build.sbt
@@ -1,6 +1,8 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
+libraryDependencies += "com.storm-enroute" %% "scalameter-core" % "0.21"
+libraryDependencies += "com.storm-enroute" %% "scalameter" % "0.21" % "test"
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -14,4 +14,22 @@ package object Matrices {
     val random = new Random()
     type Matriz =  Vector[Vector[Int]]
 
+  /** ****************************************************************************
+   * Funciones entregadas por el profesor
+   * **************************************************************************** */
+  def matrizAlAzar(long: Int, vals: Int): Matriz = {
+    //Crea una matriz de enteros cuadrada de long x long ,
+    // con valores aleatorios entre 0 y vals
+    val v = Vector.fill(long, long) {random.nextInt(vals)}
+    v
+  }
+
+  def prodEscalar(v1: Vector[Int], v2: Vector[Int]): Int = {
+    (v1 zip v2).map({case (i,j) => (i*j)}).sum
+  }
+
+  def transpuesta (m: Matriz): Matriz = {
+    val 1=m.length
+    Vector.tabulate(1,1)((i,j) => m(j)(i))
+  }
 }
