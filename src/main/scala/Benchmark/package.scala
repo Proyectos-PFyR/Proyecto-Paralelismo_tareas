5a97643ab713fb7cc6297918cd95b401c7ec4158
SakyJoestar <itzsaky@outlook.com>
2023-05-13 11:33:48 -0500

    common y Benchmark

--- src/main/scala/Benchmark/package.scala
+++ src/main/scala/Benchmark/package.scala
@@ -1,7 +1,23 @@
+import Matrices._
+import org.scalameter._
 package object Benchmark {
-  /** ****************************************************************************
-   * AUTORES
-   * Samuel Galindo Cuevas - 202177491
-   * Nicolas Herrera Marulanda - 202182551
-   * **************************************************************************** */
+  type AlgoritmoMult = (Matriz, Matriz) => Matriz
+
+  def compararAlgoritmos(a1:AlgoritmoMult, a2:AlgoritmoMult)
+                        (m1:Matriz, m2:Matriz):(Double,Double, Double) = {
+    val timeA1 = config(
+      KeyValue(Key.exec.minWarmupRuns -> 20),
+      KeyValue(Key.exec.maxWarmupRuns -> 60),
+      KeyValue(Key.verbose -> false)
+    ) withWarmer(new Warmer.Default) measure (a1(m1,m2))
+
+    val timeA2 = config(
+      KeyValue(Key.exec.minWarmupRuns -> 20),
+      KeyValue(Key.exec.maxWarmupRuns -> 60),
+      KeyValue(Key.verbose -> false)
+    ) withWarmer(new Warmer.Default) measure (a2(m1,m2))
+
+    val speedUp= timeA1.value/timeA2.value
+    (timeA1.value, timeA2.value, speedUp)
+  }
 }
