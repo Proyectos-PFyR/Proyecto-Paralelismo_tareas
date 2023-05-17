916df591b9dbdd6ab95a3db0f321edd0dcd7f86e
SakyJoestar <itzsaky@outlook.com>
2023-05-16 21:30:01 -0500

    Funciones 1.1

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -1,4 +1,4 @@
-import common._
+import common.{parallel, task}
 
 import scala.util.Random
 package object Matrices {
@@ -29,9 +29,104 @@ package object Matrices {
   }
 
   def transpuesta (m: Matriz): Matriz = {
-    val 1=m.length
-    Vector.tabulate(1,1)((i,j) => m(j)(i))
+    val l =m.length
+    Vector.tabulate(l,l)((i,j) => m(j)(i))
   }
+
+  /** ****************************************************************************
+   * FUNCIÓN:                multMatriz
+   * DESCRIPCIÓN:            multiplicacion de matrices usando prodEscalar y transpuesta
+   * PARÁMETROS DE ENTRADA
+   * m1 :                    Matriz
+   * m2 :                    Matriz
+   * RETORNO
+   * Matriz:                 Matriz resultante de la multiplicaciond de m1 y m2
+   * **************************************************************************** */
+
+  def multMatriz3(m1: Matriz, m2: Matriz): Matriz = {
+    val m2t = transpuesta(m2)
+    Vector.tabulate(m1.length, m2.length) { case (i, j) => prodEscalar(m1(i), m2t(j)) }
+  }
+
+/*
+  def multMatriz2(m1: Matriz, m2: Matriz): Matriz ={
+    val m2t = transpuesta(m2)
+    val n = m1.length
+    def auxMatriz2(s: Int, fila: Vector[Int], columna: Vector[Int] ) : Vector[Int] = {
+      if(s == n)
+    }
+    Vector.tabulate(, m2.length){case (i,j) => prodEscalar(m1(i), m2t(j))}
+  }
+
+  def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
+    def generarIJ(fila: Int, columna: Int): Int = {
+      val multi = for (x <- 0 until m1.length) yield m1(fila)(x) * m2(x)(columna)
+      multi.sum
+    }
+
+    Vector.tabulate(m1.length, m2.length) { case (i, j) => generarIJ(i, j) }
+  }
+
+ */
+
+  /** ****************************************************************************
+   * FUNCIÓN:                multMatriz
+   * DESCRIPCIÓN:            multiplicacion paralela de matrices usando prodEscalar y transpuesta
+   * PARÁMETROS DE ENTRADA
+   * m1 :                    Matriz
+   * m2 :                    Matriz
+   * RETORNO
+   * Matriz:                 Matriz resultante de la multiplicaciond de m1 y m2
+   * **************************************************************************** */
+  def multMatrizPar3(m1: Matriz, m2: Matriz): Matriz = {
+    val m2t = transpuesta(m2)
+    val limite = 2
+    val n = m1.length
+
+    def auxPar(inf: Int, sup: Int): Matriz = {
+      if (sup - inf < limite) Vector.tabulate(1, m2.length) { case (i, j) => prodEscalar(m1(inf), m2t(j)) }
+      else {
+        val m = inf + (sup - inf) / 2
+        val (x, y) = parallel(auxPar(inf, m), auxPar(m, sup))
+        x ++ y
+      }
+    }
+    if (n == 1) {
+      Vector(Vector(m1(0)(0) * m2(0)(0)))
+    }
+    else {
+      auxPar(0, m1.length)
+    }
+  }
+
+  /*
+    def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
+      val n = m1.length
+      val m2t = transpuesta(m2)
+      if (n == 1) {
+        Vector(Vector(m1(0)(0) * m2(0)(0)))
+      }
+      else
+      {
+
+        Vector.tabulate(m1.length, m2.length){case (i,j) => prodEscalar(m1(i), m2t(j))}
+      }
+
+      def dividirFilas(v1: Vector[Int], v2: Vector[Int], inf: Int, sup: Int): Int = {
+        if (inf == 1 ) prodEscalar(v1,v2)
+        else {val (x,y) = parallel(dividirFilas(v1,v2,0,sup/2), dividirFilas(v1,v2,sup/2,sup)) }
+      }
+    }
+
+    def multMatrizPar2(m1: Matriz, m2: Matriz): Matriz = {
+      val m2t = transpuesta(m2)
+      val (x,y) = parallel(Vector.tabulate(m1.length/2, m2.length) { case (i, j) => prodEscalar(m1(i), m2t(j))}, Vector.tabulate(m1.length - m1.length/2, m2.length) { case (i, j) => prodEscalar(m1(i+1), m2t(j))})
+      val matrizU = x ++ y
+      matrizU
+    }
+
+   */
+
 
   /** ****************************************************************************
    * FUNCIÓN:                subMatriz
