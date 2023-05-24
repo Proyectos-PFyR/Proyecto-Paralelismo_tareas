fe0a83dc1619d113e1c5031cfeb4adff631c4ecf
SakyJoestar <itzsaky@outlook.com>
2023-05-23 19:24:00 -0500

    Ultimo Commit

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -43,31 +43,10 @@ package object Matrices {
    * Matriz:                 Matriz resultante de la multiplicaciond de m1 y m2
    * **************************************************************************** */
 
-  def multMatriz3(m1: Matriz, m2: Matriz): Matriz = {
+  def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
     val m2t = transpuesta(m2)
     Vector.tabulate(m1.length, m2.length) { case (i, j) => prodEscalar(m1(i), m2t(j)) }
   }
-
-/*
-  def multMatriz2(m1: Matriz, m2: Matriz): Matriz ={
-    val m2t = transpuesta(m2)
-    val n = m1.length
-    def auxMatriz2(s: Int, fila: Vector[Int], columna: Vector[Int] ) : Vector[Int] = {
-      if(s == n)
-    }
-    Vector.tabulate(, m2.length){case (i,j) => prodEscalar(m1(i), m2t(j))}
-  }
-
-  def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
-    def generarIJ(fila: Int, columna: Int): Int = {
-      val multi = for (x <- 0 until m1.length) yield m1(fila)(x) * m2(x)(columna)
-      multi.sum
-    }
-
-    Vector.tabulate(m1.length, m2.length) { case (i, j) => generarIJ(i, j) }
-  }
-
- */
 
   /** ****************************************************************************
    * FUNCIÓN:                multMatriz
@@ -78,7 +57,7 @@ package object Matrices {
    * RETORNO
    * Matriz:                 Matriz resultante de la multiplicaciond de m1 y m2
    * **************************************************************************** */
-  def multMatrizPar3(m1: Matriz, m2: Matriz): Matriz = {
+  def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
     val m2t = transpuesta(m2)
     val limite = 2
     val n = m1.length
@@ -98,35 +77,6 @@ package object Matrices {
       auxPar(0, m1.length)
     }
   }
-
-  /*
-    def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
-      val n = m1.length
-      val m2t = transpuesta(m2)
-      if (n == 1) {
-        Vector(Vector(m1(0)(0) * m2(0)(0)))
-      }
-      else
-      {
-
-        Vector.tabulate(m1.length, m2.length){case (i,j) => prodEscalar(m1(i), m2t(j))}
-      }
-
-      def dividirFilas(v1: Vector[Int], v2: Vector[Int], inf: Int, sup: Int): Int = {
-        if (inf == 1 ) prodEscalar(v1,v2)
-        else {val (x,y) = parallel(dividirFilas(v1,v2,0,sup/2), dividirFilas(v1,v2,sup/2,sup)) }
-      }
-    }
-
-    def multMatrizPar2(m1: Matriz, m2: Matriz): Matriz = {
-      val m2t = transpuesta(m2)
-      val (x,y) = parallel(Vector.tabulate(m1.length/2, m2.length) { case (i, j) => prodEscalar(m1(i), m2t(j))}, Vector.tabulate(m1.length - m1.length/2, m2.length) { case (i, j) => prodEscalar(m1(i+1), m2t(j))})
-      val matrizU = x ++ y
-      matrizU
-    }
-
-   */
-
 
   /** ****************************************************************************
    * FUNCIÓN:                subMatriz
@@ -326,7 +276,7 @@ package object Matrices {
    * Matriz:  			         Matriz resultante de multiplicar m1 y m2
    * **************************************************************************** */
 
-  def multStrassenPar2(m1: Matriz, m2: Matriz): Matriz = {
+  def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
     val n = m1.length
 
     if (n == 1) {
@@ -372,53 +322,4 @@ package object Matrices {
       c11.join().zip(c12.join()).map { case (filaC11, filaC12) => filaC11 ++ filaC12 } ++ c21.join().zip(c22.join()).map { case (filaC21, filaC22) => filaC21 ++ filaC22 }
     }
   }
-  /*
-  def multStrassenPar3(m1: Matriz, m2: Matriz): Matriz = {
-    val n = m1.length
-
-    if (n == 1) {
-      Vector(Vector(m1(0)(0) * m2(0)(0)))
-    }
-    else {
-      val m = n / 2
-
-      val a11 = subMatriz(m1, 0, 0, m)
-      val a12 = subMatriz(m1, 0, m, m)
-      val a21 = subMatriz(m1, m, 0, m)
-      val a22 = subMatriz(m1, m, m, m)
-
-      val b11 = subMatriz(m2, 0, 0, m)
-      val b12 = subMatriz(m2, 0, m, m)
-      val b21 = subMatriz(m2, m, 0, m)
-      val b22 = subMatriz(m2, m, m, m)
-
-      val s1 = task(resMatriz(b12, b22))
-      val s2 = task(sumMatriz(a11, a12))
-      val s3 = task(sumMatriz(a21, a22))
-      val s4 = task(resMatriz(b21, b11))
-      val s5 = task(sumMatriz(a11, a22))
-      val s6 = task(sumMatriz(b11, b22))
-      val s7 = task(resMatriz(a12, a22))
-      val s8 = task(sumMatriz(b21, b22))
-      val s9 = task(resMatriz(a11, a21))
-      val s10 = task(sumMatriz(b11, b12))
-
-      val p1 = task(multStrassen(a11, s1.join()))
-      val p2 = task(multStrassen(s2.join(), b22))
-      val p3 = task(multStrassen(s3.join(), b11))
-      val p4 = task(multStrassen(a22, s4.join()))
-      val p5 = task(multStrassen(s5.join(), s6.join()))
-      val p6 = task(multStrassen(s7.join(), s8.join()))
-      val p7 = task(multStrassen(s9.join(), s10.join()))
-
-      val c11 = task(resMatriz(sumMatriz(p6.join(), sumMatriz(p5.join(), p4.join())), p2.join()))
-      val c12 = task(sumMatriz(p1.join(), p2.join()))
-      val c21 = task(sumMatriz(p3.join(), p4.join()))
-      val c22 = task(resMatriz(sumMatriz(p5.join(), p1.join()), sumMatriz(p3.join(), p7.join())))
-
-      c11.join().zip(c12.join()).map { case (filaC11, filaC12) => filaC11 ++ filaC12 } ++ c21.join().zip(c22.join()).map { case (filaC21, filaC22) => filaC21 ++ filaC22 }
-    }
-  }
-
-   */
 }
--- src/test/scala/Pruebas.sc
+++ src/test/scala/Pruebas.sc
@@ -1,7 +1,31 @@
-import Matrices._
 import Benchmark._
+import Matrices._
+
+/******************************************************************************
+ * FUNCIÓN:                multMatriz y multMatrizPar
+ ******************************************************************************/
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatriz, multMatrizPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatriz, multMatrizPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatriz, multMatrizPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatriz, multMatrizPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatriz, multMatrizPar)(m1, m2))
 
 /******************************************************************************
  * FUNCIÓN:                multMatrizRec y multMatrizRecPar
  ******************************************************************************/
-for(i <- 1 to 10; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multMatrizRec, multMatrizRecPar)(m1, m2))
+
+/******************************************************************************
+ * FUNCIÓN:                multStrassen y multStrassenPar
+ ******************************************************************************/
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multStrassen, multStrassenPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multStrassen, multStrassenPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multStrassen, multStrassenPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multStrassen, multStrassenPar)(m1, m2))
+for(i <- 1 to 10 ; m1 = matrizAlAzar(math.pow(2, i).toInt, 2); m2 = matrizAlAzar(math.pow(2, i).toInt, 2)) yield (compararAlgoritmos(multStrassen, multStrassenPar)(m1, m2))
+
+
